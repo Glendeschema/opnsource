60b130b68298c4aa8f67cb07e1e8155a24223148
GlenTsime <lehlohonologlen@gmail.com>
2018-11-09 08:01:10 +0200

    Code Deployment for AzureAVSet1

--- AzureWebinar/Deployment.ps1
+++ AzureWebinar/Deployment.ps1
@@ -1,5 +1,7 @@
 New-AzureRmResourceGroupDeployment -Name AVset -ResourceGroupName Test3 `
--Mode Incremental `
--TemplateFile /Users/glen/Desktop/AzureWebinar/LinuxVMcount/template.json `
--TemplateParameterFile /Users/glen/Desktop/AzureWebinar/LinuxVMcount/parameters.json `
--Verbose
+-Mode Complete `
+-TemplateUri "https://raw.githubusercontent.com/glendeschema/opnsource/master/AzureWebinar/LinuxVMcount/template.json"`
+-TemplateParameterUri "https://raw.githubusercontent.com/glendeschema/opnsource/master/AzureWebinar/LinuxVMcount/parameters.json" `
+-Verbose `
+-Force
+
